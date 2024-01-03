e91ed41a14caa3573e161f033cb4992627b93f67
Ai Niimura <an2221@ic.ac.uk>
2024-01-03 20:16:44 +0000

    changed js code locations

--- FrontEnd/Settings.js
+++ FrontEnd/Settings.js
@@ -9,9 +9,12 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const phoneNumbersInDB = ref(database, "phoneNumber")
 
-const inputFieldEl = document.getElementById("phonenb1")
-const inputField2El = document.getElementById("phonenb2")
-const inputField3El = document.getElementById("phonenb3")
+const inputContact1 = document.getElementById("phonenb1")
+const inputContact2 = document.getElementById("phonenb2")
+const inputContact3 = document.getElementById("phonenb3")
+const inputUsername = document.getElementById("usernamevar")
+const inputEmail = document.getElementById("emailvar")
+const inputPassword = document.getElementById("passwordvar")
 const updateButtonEl = document.getElementById("update-button")
 
 // Display phone numbers in input boxes when the page loads
@@ -19,16 +22,16 @@ onValue(phoneNumbersInDB, (snapshot) => {
     const data = snapshot.val();
     if (data) {
         // Update input boxes with the latest values
-        inputFieldEl.value = data.number1 || "";
-        inputField2El.value = data.number2 || "";
-        inputField3El.value = data.number3 || "";
+        inputContact1.value = data.number1 || "";
+        inputContact2.value = data.number2 || "";
+        inputContact3.value = data.number3 || "";
     }
 });
 
 updateButtonEl.addEventListener("click", function() {
-    let phoneNumber1 = inputFieldEl.value;
-    let phoneNumber2 = inputField2El.value;
-    let phoneNumber3 = inputField3El.value;
+    let phoneNumber1 = inputContact1.value;
+    let phoneNumber2 = inputContact2.value;
+    let phoneNumber3 = inputContact3.value;
 
     // Use set instead of push to update the values
     set(phoneNumbersInDB, { number1: phoneNumber1, number2: phoneNumber2, number3: phoneNumber3 });
@@ -36,7 +39,13 @@ updateButtonEl.addEventListener("click", function() {
 
 
 
-
+//link to home page
+var backPageLink = document.getElementById("backBtn");
+if (backPageLink) {
+    backPageLink.addEventListener("click", function (e) {
+        window.location.href = "./Home.html";
+    });
+}
 
 
 
