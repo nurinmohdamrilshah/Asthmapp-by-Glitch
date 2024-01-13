8abc42cc65bace581622fb86648cc011f39c9336
emmagha <146962193+eg721@users.noreply.github.com>
2024-01-13 19:20:44 +0000

    Add Crisis created page

--- FrontEnd/AddCrisis.js
+++ FrontEnd/AddCrisis.js
@@ -275,3 +275,42 @@ if (popupaddCrisisBtnContainer) {
         }
     });
 }
+
+//Navigation
+var topNav = document.getElementById("back");
+if (topNav) {
+    topNav.addEventListener("click", function (e) {
+        window.location.href = "./Emergency1.html";
+    });
+}
+
+var close = document.getElementById("close");
+if (close) {
+    close.addEventListener("click", function (e) {
+        window.location.href = "./Home.html";
+    });
+}
+
+var home = document.getElementById("homeBtn");
+if (home) {
+    home.addEventListener("click", function (e) {
+        window.location.href = "./Home.html";
+    });
+}
+
+var cloud = document.getElementById("airQltyBtn");
+if (cloud) {
+    cloud.addEventListener("click", function (e) {
+        window.location.href = "./AirQuality01.html";
+    });
+}
+
+var inhaler = document.getElementById("inhalerBtn");
+if (inhaler) {
+    inhaler.addEventListener("click", function (e) {
+        window.location.href = "./MyInhaler.html";
+    });
+}
+
+document.getElementById("closePopup")?.addEventListener("click", () => window.location.href = "./Emergency3.html");
+document.getElementById("addcrisisbtn")?.addEventListener("click", () => window.location.href = "./Emergency3.html");
