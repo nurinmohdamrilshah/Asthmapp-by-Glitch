7d291602ef48b27a2663b3969a58d383b27035eb
Nurin Nasuha <nnm121@ic.ac.uk>
2024-01-14 16:32:43 +0000

    added comments, fixed user auth

--- FrontEnd/MyUsageLog.js
+++ FrontEnd/MyUsageLog.js
@@ -14,22 +14,17 @@ function MyUsageLog(firebaseConfig) {
     const analytics = getAnalytics(app);
 
     const auth = getAuth();
-    var currentUser = auth.currentUser;
-    if (currentUser) {
-        var currentUID = currentUser.uid;
-        var currentUserDB = ref(database, '/users/' + currentUID)
-        var inhalerDB = child(currentUserDB, '/inhalers')
-    } else {
-        currentUID = 'testDosage2';
-        currentUserDB = ref(database, '/users/' + currentUID);
-        inhalerDB = child(currentUserDB, '/inhalers');
-    }
+    let currentUser
+    let currentUID
+    let currentUserDB
+    let inhalerDB
+    //identifying current logged in user
     onAuthStateChanged(auth, (user) => {
         if (user) {
-            currentUser = auth.currentUser;
+            currentUser = user
             currentUID = user.uid;
-            currentUserDB = ref(database, '/users/' + currentUID);
-            inhalerDB = child(currentUserDB, '/inhalers');
+            currentUserDB = ref(database,'/users/'+currentUID)
+            inhalerDB = ref(database,'/users/'+currentUID+'/inhalers')
         }
     })
 
@@ -131,48 +126,58 @@ function MyUsageLog(firebaseConfig) {
         });
     }
     var mainSection = document.getElementById("mainMyInhalerStats")
+    //iterating over every inhaler
     get(inhalerDB).then((snapshot) => {
         if (snapshot.exists()) {
             snapshot.forEach(function (childSnapshot) {
                 var intakesDB = child(inhalerDB, childSnapshot.val().inhaler.name + '/intakes/')
+
+                //iterating over every intake
                 get(intakesDB).then((intakeList) => {
                     if (intakeList.exists()) {
                         intakeList.forEach(function (intake) {
+                            // creating HTML element for individual intake
                             let intakeLogSection = document.createElement('ul')
                             intakeLogSection.className = "intakelogsection"
 
+                            // converting intake time of ISO string from user database to Date
                             var intakeDateFormat = new Date(intake.val().time)
+
+                            // displaying date and time of intake
                             let intakeTime = document.createElement('h1')
                             intakeTime.className = "intaketime"
                             intakeTime.textContent = intakeDateFormat.toLocaleDateString() + " at " + intakeDateFormat.toLocaleTimeString();
                             intakeLogSection.appendChild(intakeTime)
 
+                            // displaying name of the inhaler for the intake
                             let intakeLogSection1 = document.createElement('div')
                             intakeLogSection1.className = "intakelogsection1"
                             let inhalerPointer = document.createElement('h2')
                             inhalerPointer.className = "numberofpuffs"
                             inhalerPointer.textContent = "Inhaler:"
                             intakeLogSection1.appendChild(inhalerPointer)
-
                             let inhalerNameVar = document.createElement('b')
                             inhalerNameVar.className = "inhalernamevar";
                             inhalerNameVar.textContent = childSnapshot.val().inhaler.name
                             intakeLogSection1.appendChild(inhalerNameVar)
 
+                            // displaying number of puffs for the intake
                             let intakeLogSection2 = document.createElement('div')
                             intakeLogSection2.className = "intakelogsection1"
                             let puffsPointer = document.createElement('h2')
                             puffsPointer.className = "numberofpuffs"
                             puffsPointer.textContent = "Number of Puffs:"
                             intakeLogSection2.appendChild(puffsPointer)
-
                             let puffsNumVar = document.createElement('b')
                             puffsNumVar.className = "inhalernamevar";
                             puffsNumVar.textContent = intake.val().puffNum
                             intakeLogSection2.appendChild(puffsNumVar)
 
+                            //appending all sections containing inhaler info together
                             intakeLogSection.appendChild(intakeLogSection1)
                             intakeLogSection.appendChild(intakeLogSection2)
+
+                            //append log section for individual intakes on the document's main body
                             mainSection.appendChild(intakeLogSection)
                         })
                     }
