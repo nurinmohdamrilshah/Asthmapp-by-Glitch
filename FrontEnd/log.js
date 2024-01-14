369b090be91ca9f1122a1b06acf5e06c56a30650
sh1720 <sh1720@ic.ac.uk>
2024-01-14 14:46:57 +0000

    log.js change to module bulnder

--- FrontEnd/log.js
+++ FrontEnd/log.js
@@ -1,49 +1,51 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "firebase/app"
+import { getDatabase, ref, onValue } from "firebase/database"
 
-const appSettings = {
-    databaseURL: "https://asthmapp-121a8-default-rtdb.europe-west1.firebasedatabase.app/"
-}
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const crisisListInDB = ref(database, "addCrisisData")
-const crisisListEl = document.getElementById("crisis-list")
+function log(firebaseConfig){
+    const app = initializeApp(firebaseConfig)
 
-onValue(crisisListInDB, function(snapshot) {
-    if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
-    
-        clearCrisisListEl();
-        
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i];
-            let currentItemID = currentItem[0];
-            let currentItemValue = currentItem[1];
-            
-            appendItemToShoppingListEl(currentItem);
-        }    
-    } else {
-        crisisListEl.innerHTML = "No Crisis Logged in the ";
-    }
-});
+    const database = getDatabase(app)
+    const crisisListInDB = ref(database, "addCrisisData")
+    const crisisListEl = document.getElementById("crisis-list")
 
-function clearCrisisListEl() {
-    crisisListEl.innerHTML = "";
-}
+    onValue(crisisListInDB, function (snapshot) {
+        if (snapshot.exists()) {
+            let itemsArray = Object.entries(snapshot.val())
 
-function appendItemToShoppingListEl(item) {
-    let itemID = item[0];
-    let itemValue = item[1];
-    
-    let newEl = document.createElement("li");
-    
-    newEl.textContent = itemValue.dateTimeInput;
-    
-    newEl.addEventListener("click", function() {
-        // Modify this part as needed
-        window.location.href = 'DeleteCrisis.html';
+            clearCrisisListEl();
+
+            for (let i = 0; i < itemsArray.length; i++) {
+                let currentItem = itemsArray[i];
+                let currentItemID = currentItem[0];
+                let currentItemValue = currentItem[1];
+
+                appendItemToShoppingListEl(currentItem);
+            }
+        } else {
+            crisisListEl.innerHTML = "No Crisis Logged in the ";
+        }
     });
-    
-    crisisListEl.append(newEl);
+
+    function clearCrisisListEl() {
+        crisisListEl.innerHTML = "";
+    }
+
+    function appendItemToShoppingListEl(item) {
+        let itemID = item[0];
+        let itemValue = item[1];
+
+        let newEl = document.createElement("li");
+
+        newEl.textContent = itemValue.dateTimeInput;
+
+        newEl.addEventListener("click", function () {
+            // Modify this part as needed
+            window.location.href = 'DeleteCrisis.html';
+        });
+
+        crisisListEl.append(newEl);
+    }
 }
+
+export default log
