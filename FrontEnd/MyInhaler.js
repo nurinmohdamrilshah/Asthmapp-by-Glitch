2231abf1ff10f03581d497fc30df86b9a06cdf9b
emmagha <146962193+eg721@users.noreply.github.com>
2024-01-15 00:58:59 +0000

    added My Inhaler page nav

--- FrontEnd/MyInhaler.js
+++ FrontEnd/MyInhaler.js
@@ -252,13 +252,7 @@ function MyInhaler(firebaseConfig) {
     document.getElementById("addIntakeBtn")?.addEventListener("click", closePopup);
     document.getElementById("applyBtn")?.addEventListener("click", closePopup);
 
-    document.getElementById("usageHistoryBtn")?.addEventListener("click", () => window.location.href = "./MyUsageLog.html");
-    document.getElementById("newInhalerIntakeBtn")?.addEventListener("click", () => window.location.href = "./AddIntakePopup.html");
-    document.getElementById("editInhalerBtn")?.addEventListener("click", () => window.location.href = "./AddInhalerPopup.html");
-    document.getElementById("newInhalerBtn")?.addEventListener("click", () => window.location.href = "./AddInhalerPopup.html");
-    document.getElementById("homeBtn")?.addEventListener("click", () => window.location.href = "./Home.html");
-    document.getElementById("cloudContainer")?.addEventListener("click", () => window.location.href = "./AirQuality01.html");
-    document.getElementById("emergencyBtn")?.addEventListener("click", () => window.location.href = "./Emergency1.html");
+
 
 }
 
